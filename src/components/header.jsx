b6fdbf836b9dc84237ebe3d9f5b688e189b54c5d
Luis Acevedo <luisemilio.acevedo@globant.com>
2024-03-06 18:08:59 -0500

    algo de estilos a los link

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,7 +15,7 @@ export function Header(){
                         <ul className="header-nav">
                             <li><Link to="/">Overview</Link></li>
                             <li><Link to="contactos">Contacts</Link></li>
-                            <li>Favorites</li>
+                            <li style={{color:"#C1D72F"}}>Favorites</li>
                         </ul>
                         <Btn texto={"+ New"} />
                     </div>
@@ -28,7 +28,7 @@ export function Header(){
                     <div className="header-div">
                         <ul className="header-nav">
                             <li><Link to="/">Overview</Link></li>
-                            <li>Contacts</li>
+                            <li style={{color:"#C1D72F"}}>Contacts</li>
                             <li><Link to="favoritos">Favorites</Link></li>
                         </ul>
                         <Btn texto={"+ New"} />
@@ -41,7 +41,7 @@ export function Header(){
                         <img src="/img/globant.png" alt="Logo de Globant"/>
                         <div className="header-div">
                             <ul className="header-nav">
-                                <li>Overview</li>
+                                <li style={{color:"#C1D72F"}}>Overview</li>
                                 <li><Link to="contactos">Contacts</Link></li>
                                 <li><Link to="favoritos">Favorites</Link></li>
                             </ul>
